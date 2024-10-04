7c8cf4c899f3a7e2e0f68119ee0f43b6c43e17d7
Rafael Genari <rafaelg.genari@gmail.com>
2024-10-04 17:19:27 +0100

    Code adjustments

--- app.py
+++ app.py
@@ -5,19 +5,19 @@ import plotly.express as px
 car_data = pd.read_csv(
     r'C:\\Users\\Rafael\\Desktop\\Rafael\\Bootcamp\\Projects\\Bootcamp-Projects-Github\\Bootcamp-Project-Vehicles-App\\vehicles.csv')  # lendo os dados
 
-# criar um histograma
-fig = px.histogram(car_data, x="type")
-
-# exibir um gráfico Plotly interativo
-st.plotly_chart(fig, use_container_width=True)
-
-# criar um gráfico de dispersão
-fig = px.scatter(car_data, x='price', y='condition')
-
-# exibir um gráfico Plotly interativo
-st.plotly_chart(fig, use_container_width=True)
-
 tab1, tab2 = st.tabs(['Contagem dos tipos de carros', 'Condition x Price'])
+with tab1:
+    # criar um histograma
+    fig = px.histogram(car_data, x="type")
+
+    # exibir um gráfico Plotly interativo
+    st.plotly_chart(fig, use_container_width=True)
+with tab2:
+    # criar um gráfico de dispersão
+    fig = px.scatter(car_data, x='price', y='condition')
+
+    # exibir um gráfico Plotly interativo
+    st.plotly_chart(fig, use_container_width=True)
 
 build_bar_graphic = st.checkbox(
     'Criar um gráfico de barras')  # criar um gráfico de barras
