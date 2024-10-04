9b00f757783eece5e003c4f3cfa4cfe9b33ccfab
Rafael Genari <rafaelg.genari@gmail.com>
2024-10-04 17:25:30 +0100

    Code adjustments

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ if build_bar_graphic:  # se o botão for clicado
         'Criando um gráfico de barras para o conjunto de dados de anúncios de vendas de carros')
 
     # criar um gráfico de barras
-    fig = px.bar(car_data, x='model_year', y='price')
+    fig = px.bar(car_data, x='price', y='model_year')
 
     # exibir um gráfico Plotly interativo
     st.plotly_chart(fig, use_container_width=True)
