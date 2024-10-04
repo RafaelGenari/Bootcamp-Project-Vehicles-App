8141ca2c222b44e4b7ccc91be4f3ee25d7a8845c
Rafael Genari <rafaelg.genari@gmail.com>
2024-10-04 17:01:16 +0100

    Adding filters

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ import plotly.express as px
 
 car_data = pd.read_csv(
     r'C:\\Users\\Rafael\\Desktop\\Rafael\\Bootcamp\\Projects\\Bootcamp-Projects-Github\\Bootcamp-Project-Vehicles-App\\vehicles.csv')  # lendo os dados
+
+# filtro para os dados de vendas de carros
+columns_group = [price, model_year, model, condition, cylinders, fuel,
+                 odometer, transmission, type, paint_color, is_4wd, date_posted, days_listed]
+
 hist_button = st.button('Criar histograma')  # criar um botão
 
 if hist_button:  # se o botão for clicado
@@ -12,7 +17,7 @@ if hist_button:  # se o botão for clicado
         'Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
 
     # criar um histograma
-    fig = px.histogram(car_data, x="odometer")
+    fig = px.histogram(car_data, columns_group)
 
     # exibir um gráfico Plotly interativo
     st.plotly_chart(fig, use_container_width=True)
@@ -25,7 +30,7 @@ if scatter_button:  # se o botão for clicado
         'Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros')
 
     # criar um gráfico de dispersão
-    fig = px.scatter(car_data, x="odometer")
+    fig = px.scatter(car_data, columns_group)
 
     # exibir um gráfico Plotly interativo
     st.plotly_chart(fig, use_container_width=True)
@@ -39,7 +44,7 @@ if build_bar_graphic:  # se o botão for clicado
         'Criando um gráfico de barras para o conjunto de dados de anúncios de vendas de carros')
 
     # criar um gráfico de barras
-    fig = px.bar(car_data, x="odometer")
+    fig = px.bar(car_data, columns_group)
 
     # exibir um gráfico Plotly interativo
     st.plotly_chart(fig, use_container_width=True)
